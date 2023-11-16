861a4bed78b31b2f896d304d1d867ca1fc6343a8
matheus-kiyoshi <matheus.kiyoshi.f@gmail.com>
2023-11-16 15:21:37 -0300

    fix: email sender

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,8 +8,34 @@ import * as crypto from 'crypto';
 import { Prisma } from '@prisma/client';
 import { UpdateUserPasswordDto } from './dto/update-user-password';
 import { ResetPasswordUserDto } from './dto/reset-password-user.dto';
-import { sendEmail } from 'src/utils/email/sendEmail.util';
+import * as nodemailer from 'nodemailer';
 import { prismaExclude } from 'src/utils/exclude/exclude-entity-attribute.util';
+
+export async function sendEmail(userEmail: string, subject: string, text: string, html: string) {
+	const transporter = nodemailer.createTransport({
+		service: 'Outlook',
+		auth: {
+			user: process.env.EMAIL_USER,
+			pass: process.env.EMAIL_PASS,
+		},
+	})
+
+	const mailOptions = {
+		from: process.env.EMAIL_USER,
+		to: userEmail,
+		subject: subject,
+		text: text,
+		html: html,
+	}
+	
+	return transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			console.error('Error', error)
+		} else {
+			console.log('Email sent', info.response)
+		}
+	})
+}
 
 @Injectable()
 export class UserService {
